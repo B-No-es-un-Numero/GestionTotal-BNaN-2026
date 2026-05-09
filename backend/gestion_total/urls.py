9233a9c322f71829826872957be76a471f1ca00e
Bruvm <melibruvera@hotmail.com>
2026-05-09 17:31:04 -0300

    User Crud was created

--- backend/gestion_total/urls.py
+++ backend/gestion_total/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [ path('', include('client_app.urls')), 
                path('admin/', admin.site.urls),
                path('api/', include('client_app.urls')), 
-               path('api/', include('company_app.urls')), ]
+               path('api/', include('company_app.urls')), 
+               path('api/', include('user_app.urls')), ]
