7732af1fb0f6c6a635dc734861f6963b5599232a
Guille\Guillermo <guillermodivan@hotmail.com>
2026-05-19 18:42:06 -0300

    hotfix(users):back-nnn-duplicated-users-fix

--- backend/gestion_total/urls.py
+++ backend/gestion_total/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [ path('admin/', admin.site.urls),
                path('api/', include('client_app.urls')), 
                path('api/', include('company_app.urls')), 
                path('api/', include('user_app.urls')),
-               path('api/', include('task_app.urls')),
-               path('api/auth/', include('users.urls')),]
+               path('api/', include('task_app.urls')),]
 
