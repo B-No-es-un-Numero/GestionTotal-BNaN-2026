f5795832ca78f78724d68a6a76762c6344b7143b
Guille\Guillermo <guillermodivan@hotmail.com>
2026-05-20 15:26:21 -0300

    feat(front):120-121-122-get all, get by and post CLIENT working

--- frontend/bnan-project/src/app/pages/user-dashboard/client-list-view/client-list-view.ts
+++ frontend/bnan-project/src/app/pages/user-dashboard/client-list-view/client-list-view.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ClientService } from '../../../services/client-service';
 
@@ -8,19 +8,18 @@ import { ClientService } from '../../../services/client-service';
   templateUrl: './client-list-view.html',
   styleUrl: './client-list-view.css',
 })
-export class ClientListView {
-   clientList: any;
+export class ClientListView implements OnInit {
+   clientList= signal<any>({} as any);
    private clientService= inject(ClientService);
 
-  constructor(){
-    this.clientList = this.clientService.getClientList().subscribe({
+  ngOnInit(): void {
+    this.clientService.getClientList().subscribe({
       next: (data) => {
-        this.clientList=data;
+        this.clientList.set(data);
       },
       error: (error) => console.error(error),
       complete: () => console.info('complete')
     }) 
 
-  }
-
+}
 }
